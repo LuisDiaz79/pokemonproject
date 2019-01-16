85ed72e67886aeccc0c7d5a007718b03f5b20b82
mitali2190 <contractormitali21@gmail.com>
2019-01-15 17:20:10 -0800

    adding game

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 // import '../node_modules/bootstrap/dist/css/bootstrap-theme.min.css';
 // import './index.css';
 
+import {Home} from './pages/Home';
 import Game from './pages/Game';
 import Login from './components/Login';
 import Register from './components/Register';
@@ -13,6 +14,7 @@ function App() {
   return (
     <Router>
       <div>
+        <Route path='/' component={Home} />
         <Route path='/game' component={Game} />
         <Route path='/login' component={Login} />
         <Route path='/register' component={Register} />
--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -34,32 +34,64 @@ class Home extends Component {
   render() {
     return (
       <div className="container">
-        <div className="panel panel-default">
-          <div className="panel-heading">
-            <h3 className="panel-title">
-              BOOK CATALOG &nbsp;
-              {localStorage.getItem('jwtToken') &&
-                <button className="btn btn-primary" onClick={this.logout}>Logout</button>
-              }
-            </h3>
-          </div>
-          <div className="panel-body">
-            <table className="table table-stripe">
-              <thead>
-                <tr>
-                  <th>ISBN</th>
-                </tr>
-              </thead>
-              <tbody>
-                {this.state.pokemons.map(pokemons =>
-                  <tr>
-                    <td>{pokemons.name}</td>
-                  </tr>
-                )}
-              </tbody>
-            </table>
-          </div>
+        <div className="game">
+  <div className="opponent">
+    <div className="stats">
+      <div className="top">
+        <div className="pokeballs">
+          <div className="pokeball"></div>
+          <div className="pokeball"></div>
+          <div className="pokeball"></div>
+          <div className="pokeball"></div>
+          <div className="pokeball"></div>
         </div>
+        <div id = "apHP" className="hp-count">100</div>
+      </div>
+      <span className="name">
+        Charizard
+      </span>
+      <span className="level">
+        86
+      </span>
+    </div>
+    <img className="pokemon" src="http://img.pokemondb.net/sprites/black-white/anim/normal/charizard.gif" alt ="A sprite of charizard" />
+  </div>
+  <div className="player">
+    <div className="stats">
+      <div className="top">
+        <div className="pokeballs">
+          <div className="pokeball"></div>
+          <div className="pokeball"></div>
+          <div className="pokeball"></div>
+          <div className="pokeball"></div>
+          <div className="pokeball"></div>
+        </div>
+        <div id = "myHP" className="hp-count">100</div>
+      </div>
+      <span className="name">
+        Blastoise
+      </span>
+      <span className="level">
+        86
+      </span>
+    </div>
+    <img className="pokemon" src="http://bit.ly/blastoisegif" alt="A gif from blastoises back sprite" />
+  </div>
+</div>
+<div className="box">
+  <div id = "message" className="message">
+    What should Blastoise do?
+  </div>
+  <div className="actions">
+    <button onclick = "waterCannon()">Water Cannon</button>
+    <button onclick = "waterPulse()">Water Pulse</button>
+    <button onclick = "surf()">Surf</button>
+    <button onclick = "tackle()">Tackle</button>
+  </div>
+  <div className = "continue">
+    <button onclick = "compPokemon()">Continue</button>
+  </div>
+</div>
       </div>
     );
   }
