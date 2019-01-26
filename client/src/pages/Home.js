24c0eb9a15b2e48f1df5c0b9dfe31566de0adaef
mitali2190 <contractormitali21@gmail.com>
2019-01-26 11:43:19 -0800

    working

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,26 +5,29 @@ export function Home() {
     <div>
       <div id="fade">
         <header>
-          <img src="assets/pokemon_logo.png"/>
+          <img className ="imag1" src="/assets/images/pokemon2_logo.png"/>
         </header>
-          <section class="container">
+          <section className="container">
             <center>
               <input type="text" id='title' placeholder="Enter In Pokedex Number"/><br/>
-                <button id='call' type="submit">Pokedex</button>
-                <button id='battle' type="submit">Battle!!</button>
+                <button className="button1" id='call' type="submit">Pokedex</button>
+                <button className="button1" id='battle' type="submit">Battle!!</button>
                 <br/>
             </center>
-            <div class='results col-xs-offset-1 col-xs-4 '>
-                <h3>Top 5 Player</h3>s
+            <div className="row">
+            <div className='results col-4 '>
+                <h3>Top 5 Player</h3>
                 <div id='results'>
                 </div>
             </div>
-            <div class='enemy col-xs-offset-2 col-xs-4'>
+            <div className='enemy offset-2 col-4'>
               <h3>Opponent:</h3>
               <div id='enemy'></div>
             </div>
+            </div>
            </section>
       </div>
+      
     </div>
    
   );
